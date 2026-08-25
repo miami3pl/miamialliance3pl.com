0365afececa9270349360d2fc2f3c954b99ce9aa
Yuri Petrini <yuri@megalopolisms.com>
2026-08-25 12:31:06 -0500

    Add inbound freight appointment scheduling blog guide

--- js/lang/es.js
+++ js/lang/es.js
@@ -596,7 +596,7 @@ window.MA3PL_TRANSLATIONS.es = {
   "blog.title": "Blog de Perspectivas 3PL y Logística",
   "blog.subtitle":
     "Guías de expertos, análisis de la industria y consejos prácticos para marcas de e-commerce que navegan el almacenamiento, fulfillment y logística de terceros.",
-  "blog.count": "139 Artículos",
+  "blog.count": "140 Artículos",
   "blog.readarticle": "Leer Artículo →",
   "blog.minread": "min de lectura",
 
